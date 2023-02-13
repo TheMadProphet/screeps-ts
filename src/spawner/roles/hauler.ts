5d9e55e22200cb3934c21c06f3ebd7d87e730a2f
TheMadProphet <dkipi19@freeuni.edu.ge>
2023-02-14 01:22:12 +0400

    Use constant for distance in `Hauler` spawner calculations

--- src/spawner/roles/hauler.ts
+++ src/spawner/roles/hauler.ts
@@ -1,13 +1,10 @@
 import Body from "../body";
-import {HARVESTER, HAULER, MINER} from "../../constants";
+import {HAULER, MINER} from "../../constants";
+
+const DISTANCE_TEMP = 15;
 
 const haulerSpawner: RoleSpawner = {
     spawn(spawner: StructureSpawn) {
-        let distanceToSpawns = 0;
-        _.forEach(spawner.room.memory.sources, source => {
-            distanceToSpawns += (source as SourceMemory).distanceToSpawn ?? 0;
-        })
-
         const totalWorkParts = spawner.creepsByRole[MINER].reduce(
             (acc, miner) => acc + miner.getActiveBodyparts(WORK),
             0
@@ -15,7 +12,7 @@ const haulerSpawner: RoleSpawner = {
 
         const body = new Body(spawner).addParts([CARRY, MOVE], 10);
         const energyGeneratedByWorkerPerLifetime = totalWorkParts * 2 * CREEP_LIFE_TIME;
-        const biRoutePerLifetime = CREEP_LIFE_TIME / distanceToSpawns / 2;
+        const biRoutePerLifetime = CREEP_LIFE_TIME / DISTANCE_TEMP / 2;
         const energyStoredByHaulerPerLifetime = body.getCapacity() * biRoutePerLifetime;
         const requiredHaulerCount = energyGeneratedByWorkerPerLifetime / energyStoredByHaulerPerLifetime;
 
