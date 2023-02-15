9f96607c74a3afc98ce1074b883cf0fdd9f42cde
TheMadProphet <dim.kipiani@gmail.com>
2023-02-15 14:40:49 +0400

    Spawn more upgraders for RCL 1

--- src/spawner/roles/upgrader.ts
+++ src/spawner/roles/upgrader.ts
@@ -14,13 +14,9 @@ const upgraderSpawner: RoleSpawner = {
 
         const upgraders = spawner.creepsByRole[UPGRADER];
         let maxUpgraders = 3 + extraCreepCountForDistance(spawner.pos, controller.pos);
-        if (
-            controller.level === 1 ||
-            spawner.room.availableExtension > 0 ||
-            spawner.room.constructionSites.length > 0
-        ) {
+        if (spawner.room.availableExtension > 0 || spawner.room.constructionSites.length > 0) {
             maxUpgraders = 1;
-        } else if (controller.level === 2) {
+        } else if (controller.level <= 2) {
             maxUpgraders = 3;
         }
 
