5d60905b8b5c10a4862f46d060dc2be56df03ab3
TheMadProphet <dim.kipiani@gmail.com>
2023-02-27 20:58:23 +0400

    Remove intent cost when monitoring pathfinding

--- src/stats/pathfindingMonitor.ts
+++ src/stats/pathfindingMonitor.ts
@@ -6,6 +6,12 @@ Creep.prototype.travelTo = function (this: any, ...params: any) {
     const cpuStart = Game.cpu.getUsed();
     const returnValue = originalTravelTo.apply(this, params);
 
-    Statistics.registerPathfindingCpuUsage(Game.cpu.getUsed() - cpuStart);
+    let usedCpu = Game.cpu.getUsed() - cpuStart;
+    if (returnValue === OK) {
+        usedCpu -= 0.2;
+    }
+
+    Statistics.registerPathfindingCpuUsage(usedCpu);
+
     return returnValue;
 };
