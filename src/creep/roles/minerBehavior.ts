53ed65b3a19300fd4ddfcbbe9775ebd1e5262a83
TheMadProphet <dim.kipiani@gmail.com>
2023-03-19 17:37:37 +0400

    Make miners stand on containers if source has one

--- src/creep/roles/minerBehavior.ts
+++ src/creep/roles/minerBehavior.ts
@@ -1,5 +1,5 @@
-const minerBehavior: RoleBehavior = {
-    run: function (creep: Creep) {
+class MinerBehavior implements RoleBehavior {
+    run(creep: Creep) {
         if (!creep.memory.assignedSource) {
             creep.say("⚠");
             return;
@@ -8,12 +8,27 @@ const minerBehavior: RoleBehavior = {
         if (creep.memory.assignedRoom != creep.room.name) {
             creep.moveToAssignedRoom();
         } else {
-            const source = Game.getObjectById(creep.memory.assignedSource!)!;
+            const source = Game.getObjectById(creep.memory.assignedSource);
+            if (source) {
+                this.mineSource(creep, source);
+            }
+        }
+    }
+
+    private mineSource(creep: Creep, source: Source) {
+        if (source.container) {
+            if (creep.pos.isEqualTo(source.container.pos)) {
+                creep.harvest(source);
+            } else {
+                creep.travelTo(source.container);
+            }
+        } else {
             if (creep.harvestFrom(source) === ERR_NOT_ENOUGH_RESOURCES) {
                 creep.say("🕑");
             }
         }
     }
-};
+}
 
+const minerBehavior = new MinerBehavior();
 export default minerBehavior;
