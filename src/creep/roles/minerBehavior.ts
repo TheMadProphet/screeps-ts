ce62ec2c176486c50cd0b479e548410001c66f10
TheMadProphet <dkipi19@freeuni.edu.ge>
2023-03-27 03:01:19 +0400

    [Miner] Improve infrastructure maintenance code

--- src/creep/roles/minerBehavior.ts
+++ src/creep/roles/minerBehavior.ts
@@ -10,29 +10,38 @@ class MinerBehavior implements RoleBehavior {
         } else {
             const source = Game.getObjectById(creep.memory.assignedSource);
             if (source) {
-                this.maintainContainer(creep, source);
                 this.mineSource(creep, source);
             }
         }
     }
 
     private mineSource(creep: Creep, source: Source) {
-        if (source.container) {
-            if (creep.pos.isEqualTo(source.container.pos)) {
-                creep.harvest(source);
-            } else {
-                creep.travelTo(source.container);
-            }
-        } else {
-            if (creep.harvestFrom(source) === ERR_NOT_ENOUGH_RESOURCES) {
+        if (creep.pos.isNearTo(source)) {
+            if (creep.harvest(source) === ERR_NOT_ENOUGH_RESOURCES) {
+                this.maintainInfrastructure(creep, source);
                 creep.say("🕑");
             }
+        } else {
+            if (source.container) creep.travelTo(source.container);
+            else creep.travelTo(source, {range: 1});
         }
     }
 
-    private maintainContainer(creep: Creep, source: Source) {
-        if (source.container && creep.store.getUsedCapacity() > 0) {
+    private maintainInfrastructure(creep: Creep, source: Source) {
+        if (!source.container) return;
+
+        if (creep.store.getUsedCapacity() === 0) {
+            creep.withdrawFrom(source.container);
+        } else if (source.container.hits / source.container.hitsMax < 0.95) {
             creep.repair(source.container);
+        } else {
+            const damagedRoads = creep.pos.findInRange(FIND_STRUCTURES, 3, {
+                filter: it => it.structureType === STRUCTURE_ROAD && it.hitsMax - it.hits >= 500
+            });
+
+            if (damagedRoads.length) {
+                creep.repair(damagedRoads[0]);
+            }
         }
     }
 }
