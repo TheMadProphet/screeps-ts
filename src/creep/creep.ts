4dbcbc83062f1c61eb7e18c144cf9188242fcf5d
TheMadProphet <dkipi19@freeuni.edu.ge>
2023-04-07 23:17:15 +0400

    [Creep] Extend prototype using decorators

--- src/creep/creep.ts
+++ src/creep/creep.ts
@@ -19,15 +19,17 @@ const roleBehaviors: Record<CreepRole, RoleBehavior> = {
     [DEFENDER]: defenderBehavior
 };
 
-(function (this: typeof Creep.prototype) {
-    this.runRole = function () {
+class ExtendedCreep extends Creep {
+    @AddToPrototype
+    runRole() {
         roleBehaviors[this.memory.role].run(this);
 
         this.memory.previousPos = this.pos;
         this.memory.previousFatigue = this.fatigue;
-    };
+    }
 
-    this.idle = function () {
+    @AddToPrototype
+    idle() {
         const afkFlag = Game.flags["AFK"];
         if (afkFlag) {
             this.travelTo(afkFlag, {ignoreCreeps: false});
@@ -35,9 +37,10 @@ const roleBehaviors: Record<CreepRole, RoleBehavior> = {
 
         this.say("💤");
         this.giveWay();
-    };
+    }
 
-    this.withdrawEnergy = function (includeSpawn = true) {
+    @AddToPrototype
+    withdrawEnergy(includeSpawn = true) {
         if (!this.isHome()) {
             this.travelToHome();
             return;
@@ -54,9 +57,10 @@ const roleBehaviors: Record<CreepRole, RoleBehavior> = {
                 this.idle();
             }
         }
-    };
+    }
 
-    this.findEnergyRepository = function (includeSpawn = true) {
+    @AddToPrototype
+    findEnergyRepository(includeSpawn = true) {
         if (this.room.fillersAreEnabled() && this.room.storage) return this.room.storage;
 
         const closestContainerWithEnergy = this.pos.findClosestByRange(FIND_STRUCTURES, {
@@ -70,42 +74,47 @@ const roleBehaviors: Record<CreepRole, RoleBehavior> = {
         }
 
         return null;
-    };
+    }
 
-    this.harvestFrom = function (target: Source) {
+    @AddToPrototype
+    harvestFrom(target: Source) {
         const harvestStatus = this.harvest(target);
         if (harvestStatus === ERR_NOT_IN_RANGE) {
             this.travelTo(target);
         }
 
         return harvestStatus;
-    };
+    }
 
-    this.withdrawFrom = function (target, resource = RESOURCE_ENERGY, amount = undefined) {
+    @AddToPrototype
+    withdrawFrom(target: Structure, resource = RESOURCE_ENERGY, amount = undefined) {
         const withdrawStatus = this.withdraw(target, resource, amount);
         if (withdrawStatus === ERR_NOT_IN_RANGE) {
             this.travelTo(target);
         }
 
         return withdrawStatus;
-    };
+    }
 
-    this.pickupResource = function (resource: Resource) {
+    @AddToPrototype
+    pickupResource(resource: Resource) {
         const pickupStatus = this.pickup(resource);
         if (pickupStatus === ERR_NOT_IN_RANGE) {
             this.travelTo(resource);
         }
 
         return pickupStatus;
-    };
+    }
 
-    this.transferTo = function (target, resource = RESOURCE_ENERGY) {
+    @AddToPrototype
+    transferTo(target: Structure, resource = RESOURCE_ENERGY) {
         if (this.transfer(target, resource) === ERR_NOT_IN_RANGE) {
             this.travelTo(target);
         }
-    };
+    }
 
-    this.fillSpawnsWithEnergy = function () {
+    @AddToPrototype
+    fillSpawnsWithEnergy() {
         const closestStructure = this.pos.findClosestByRange(FIND_MY_STRUCTURES, {
             filter: structure =>
                 (structure.structureType === STRUCTURE_EXTENSION ||
@@ -130,37 +139,49 @@ const roleBehaviors: Record<CreepRole, RoleBehavior> = {
                 if (secondClosestStructure) this.travelTo(secondClosestStructure);
             }
         }
-    };
+    }
 
-    this.movedLastTick = function () {
+    @AddToPrototype
+    movedLastTick() {
         if (!this.memory.previousPos) return true;
 
         return this.pos.x !== this.memory.previousPos.x || this.pos.y !== this.memory.previousPos.y;
-    };
+    }
 
-    this.isHome = function () {
+    @AddToPrototype
+    isHome() {
         return this.room.name === this.memory.home;
-    };
+    }
 
-    this.isInAssignedRoom = function () {
+    @AddToPrototype
+    isInAssignedRoom() {
         if (!this.memory.assignedRoom) return true;
 
         return this.room.name === this.memory.assignedRoom;
-    };
+    }
 
-    this.travelToHome = function () {
+    @AddToPrototype
+    travelToHome() {
         if (this.isHome()) {
             return;
         }
 
         this.travelTo(new RoomPosition(25, 25, this.memory.home), {range: 20});
-    };
+    }
 
-    this.travelToAssignedRoom = function () {
+    @AddToPrototype
+    travelToAssignedRoom() {
         if (!this.memory.assignedRoom || this.isInAssignedRoom()) {
             return;
         }
 
         this.travelTo(new RoomPosition(25, 25, this.memory.assignedRoom), {range: 20});
+    }
+}
+
+function AddToPrototype(target: any, methodName: string, descriptor: PropertyDescriptor) {
+    // @ts-ignore
+    Creep.prototype[methodName] = function (...args: any[]) {
+        descriptor.value.apply(this, args);
     };
-}).call(Creep.prototype);
+}
