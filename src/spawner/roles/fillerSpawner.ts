7da3cce589c802a5615fc93a97f363ab14cd683f
TheMadProphet <dkipi19@freeuni.edu.ge>
2023-04-20 00:34:47 +0400

    [Filler] Increase max fillers to 2

--- src/spawner/roles/fillerSpawner.ts
+++ src/spawner/roles/fillerSpawner.ts
@@ -5,11 +5,7 @@ const fillerSpawner: RoleSpawner = {
     spawn(spawner: StructureSpawn) {
         if (spawner.room.fillersAreEnabled()) {
             const fillers = spawner.room.creepsByRole[FILLER];
-            if (fillers.length > 1) return;
-
-            const mainFiller = fillers[0];
-
-            if (!mainFiller || (mainFiller.ticksToLive && mainFiller.ticksToLive < 100)) {
+            if (fillers.length < 2) {
                 spawner.spawn({
                     body: new Body(spawner).addParts([CARRY, CARRY, MOVE], 10),
                     memory: {
