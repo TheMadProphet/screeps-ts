5fe7f19ded7484cede5e7fc014bab6f572ee09ac
TheMadProphet <TheMadProphet@users.noreply.github.com>
2026-01-18 14:40:28 +0400

    Spawn filler a bit earlier to make sure FILLER_AMOUNT is always present

--- src/spawner/roles/fillerSpawner.ts
+++ src/spawner/roles/fillerSpawner.ts
@@ -1,11 +1,16 @@
 import Body from "../body";
 import {FILLER} from "../../constants";
 
+const FILLER_AMOUNT = 1;
+
 const fillerSpawner: RoleSpawner = {
     spawn(spawner: StructureSpawn) {
         if (spawner.room.fillersAreEnabled()) {
             const fillers = spawner.room.creepsByRole[FILLER];
-            if (fillers.length < 1) {
+            const isFillerAboutToDie = fillers.some(
+                filler => filler.ticksToLive !== undefined && filler.ticksToLive! < 50
+            );
+            if (fillers.length < FILLER_AMOUNT || isFillerAboutToDie) {
                 spawner.spawn({
                     body: new Body(spawner).addParts([CARRY, CARRY, MOVE], 10),
                     memory: {
