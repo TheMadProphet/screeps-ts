5deaa83ddc3c496bb07d88e2ebe9718a6ed1bad0
TheMadProphet <TheMadProphet@users.noreply.github.com>
2026-01-20 01:33:53 +0400

    Spawn ranged defender at some point

--- src/spawner/roles/defenderSpawner.ts
+++ src/spawner/roles/defenderSpawner.ts
@@ -5,12 +5,21 @@ import {DEFENDER} from "../../constants";
 const defenderSpawner: RoleSpawner = {
     spawn(spawner: StructureSpawn) {
         if (roomDefense.needsDefenseCreep(spawner.room)) {
-            spawner.spawn({
-                body: new Body(spawner).addParts([ATTACK, MOVE], 10),
-                memory: {
-                    role: DEFENDER
-                }
-            });
+            if (spawner.room.energyCapacityAvailable > 1500) {
+                spawner.spawn({
+                    body: new Body(spawner).addParts([RANGED_ATTACK, MOVE], 10),
+                    memory: {
+                        role: DEFENDER
+                    }
+                });
+            } else {
+                spawner.spawn({
+                    body: new Body(spawner).addParts([ATTACK, MOVE], 10),
+                    memory: {
+                        role: DEFENDER
+                    }
+                });
+            }
         }
     }
 };
