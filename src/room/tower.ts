6541cebe2e5a62b13a2dcfb0dcdaa5b2ff9375ea
TheMadProphet <TheMadProphet@users.noreply.github.com>
2026-01-19 20:55:06 +0400

    Priority: attack > heal > repair
    Calculate repair amount to avoid over-repair

--- src/room/tower.ts
+++ src/room/tower.ts
@@ -5,24 +5,51 @@ declare global {
         automate(attackOnlyInvaders?: boolean): void;
 
         autoRepair(): void;
+
+        repairAmount(structure: AnyStructure): number;
     }
 }
 
 (function (this: typeof StructureTower.prototype) {
     this.automate = function (attackOnlyInvaders) {
+        const closestHostile = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {
+            filter: it => attackOnlyInvaders || it.owner.username === "Invader"
+        });
+        if (closestHostile) {
+            this.attack(closestHostile);
+            return;
+        }
+
         const damagedCreeps = this.room.find(FIND_MY_CREEPS, {filter: it => it.hits != it.hitsMax});
         if (damagedCreeps.length) {
             this.heal(damagedCreeps[0]);
             return;
         }
 
-        const closestHostile = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {
-            filter: it => attackOnlyInvaders || it.owner.username === "Invader"
+        const closestDamagedStructure = this.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: structure =>
+                structure.structureType !== STRUCTURE_WALL &&
+                structure.hitsMax - structure.hits >= this.repairAmount(structure)
         });
-
-        if (closestHostile) {
-            this.attack(closestHostile);
+        if (closestDamagedStructure) {
+            this.repair(closestDamagedStructure);
         }
+    };
+
+    this.repairAmount = function (structure: AnyStructure) {
+        const distance = this.pos.getRangeTo(structure.pos);
+        let repairPower = TOWER_POWER_REPAIR;
+
+        if (distance > TOWER_OPTIMAL_RANGE) {
+            if (distance >= TOWER_FALLOFF_RANGE) {
+                repairPower *= 1 - TOWER_FALLOFF;
+            } else {
+                const falloffFactor = (distance - TOWER_OPTIMAL_RANGE) / (TOWER_FALLOFF_RANGE - TOWER_OPTIMAL_RANGE);
+                repairPower *= 1 - TOWER_FALLOFF * falloffFactor;
+            }
+        }
+
+        return Math.floor(repairPower);
     };
 
     this.autoRepair = function () {
