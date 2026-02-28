39a35a915c50a70fdab7d0779384c2c88aca0324
TheMadProphet <TheMadProphet@users.noreply.github.com>
2026-02-28 11:24:43 +0400

    Simplify reserver spawning logic by removing unnecessary role checks

--- src/spawner/roles/reserverSpawner.ts
+++ src/spawner/roles/reserverSpawner.ts
@@ -1,13 +1,11 @@
 import Body from "../body";
-import {HAULER, MINER, RESERVER} from "../../constants";
+import {RESERVER} from "../../constants";
 import roomReserver from "../../creep/roomReserver";
 
 const reserverSpawner: RoleSpawner = {
     spawn(spawner: StructureSpawn) {
         const roomToReserve = roomReserver.getUnreservedRoomAround(spawner.room);
-        const creepsByRole = spawner.room.creepsByRole;
-
-        if (roomToReserve && creepsByRole[MINER].length > 2 && creepsByRole[HAULER].length > 2) {
+        if (roomToReserve) {
             spawner.spawn({
                 body: new Body(spawner).addParts([CLAIM, MOVE]),
                 memory: {
