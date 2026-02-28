2f10e4572c632600476cd9a52d5394d78ce1804e
TheMadProphet <TheMadProphet@users.noreply.github.com>
2026-02-28 11:24:17 +0400

    Refactor miner spawn

--- src/spawner/roles/minerSpawner.ts
+++ src/spawner/roles/minerSpawner.ts
@@ -5,17 +5,18 @@ class MinerSpawner implements RoleSpawner {
     spawn(spawner: StructureSpawn) {
         const sourceId = this.findSourceWithMissingMiner(spawner.room.memory.sources, spawner);
         if (sourceId) {
-            let body = new Body(spawner).addParts([WORK, WORK, MOVE], 3);
+            let body = this.getNormalBody(spawner);
 
             const source = Game.getObjectById(sourceId);
             if (source?.link && source.memory.linkMiningPos) {
-                body = new Body(spawner).addParts([WORK], 10).addParts([CARRY], 2).addParts([MOVE], 5);
+                body = this.getLinkBody(spawner);
             }
 
             spawner.spawn({
                 body: body,
                 memory: {role: MINER, assignedSource: sourceId, assignedRoom: spawner.room.name}
             });
+
             return;
         }
 
@@ -25,17 +26,11 @@ class MinerSpawner implements RoleSpawner {
             const isRoomReserved = Game.rooms[colony]?.isBeingReserved();
             const sourceId = this.findSourceWithMissingMiner(Memory.rooms[colony].sources, spawner, isRoomReserved);
             if (sourceId) {
-                let body = new Body(spawner).addParts([WORK, WORK, MOVE], 3);
-                if (!isRoomReserved && spawner.room.controller!.level < 4) {
-                    body = new Body(spawner).addParts([WORK, WORK, MOVE]).addParts([WORK, MOVE]);
-                } else if (Memory.sources[sourceId].containerId) {
-                    body.addParts([WORK, CARRY, MOVE]);
-                }
-
                 spawner.spawn({
-                    body: body,
+                    body: this.getRemoteBody(spawner, colony, sourceId),
                     memory: {role: MINER, assignedSource: sourceId as Id<Source>, assignedRoom: colony}
                 });
+                return;
             }
         }
     }
@@ -58,9 +53,51 @@ class MinerSpawner implements RoleSpawner {
                     return sourceId;
                 }
             }
+
+            if (this.minerWillDieSoon(spawner, assignedMiners)) {
+                return sourceId;
+            }
         }
 
         return undefined;
+    }
+
+    private minerWillDieSoon(spawner: StructureSpawn, assignedMiners: Creep[]) {
+        for (const miner of assignedMiners) {
+            if (miner.spawning) continue;
+
+            const ticksToLive = miner.ticksToLive ?? 0;
+            const sourceId = miner.memory.assignedSource!;
+            let body = this.getNormalBody(spawner);
+            if (Memory.sources[sourceId].linkId && Memory.sources[sourceId].linkMiningPos) {
+                body = this.getLinkBody(spawner);
+            }
+
+            if (ticksToLive < body.getParts().length * CREEP_SPAWN_TIME) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private getNormalBody(spawner: StructureSpawn) {
+        return new Body(spawner).addParts([WORK, WORK, MOVE], 3);
+    }
+
+    private getLinkBody(spawner: StructureSpawn) {
+        return new Body(spawner).addParts([WORK], 10).addParts([CARRY], 2).addParts([MOVE], 5);
+    }
+
+    private getRemoteBody(spawner: StructureSpawn, colony: string, sourceId: Id<Source>) {
+        const isRoomReserved = Game.rooms[colony]?.isBeingReserved();
+        if (!isRoomReserved && spawner.room.controller!.level < 4)
+            return new Body(spawner).addParts([WORK, WORK, MOVE]).addParts([WORK, MOVE]);
+
+        let body = this.getNormalBody(spawner);
+        if (Memory.sources[sourceId].containerId) body.addParts([WORK, CARRY, MOVE]);
+
+        return body;
     }
 }
 
